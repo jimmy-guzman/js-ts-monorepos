90ca18c8407fa617ccb470d0ada405321c1c0694
jimmy-guzman <jimmy.guzman@outlook.com>
2020-12-21 14:35:02 -0600

    chore: fixed commitlint setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,2 +1,7 @@
 // eslint-disable-next-line no-undef
-module.exports = { extends: ['@commitlint/config-lerna-scopes'] }
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-lerna-scopes',
+  ],
+}
