04adec32fd6057147e3cbee70ad6f915e84cd0a9
jimmy-guzman <jimmy.guzman@outlook.com>
2020-12-23 14:32:12 -0600

    fix(data): remove debugger

--- packages/data/src/messages.ts
+++ packages/data/src/messages.ts
@@ -1,24 +1,23 @@
-import { isMessage, isTypedArray, IMessage } from "@shlack/types";
-import { apiCall } from "@shlack/utils";
+import { isMessage, isTypedArray, IMessage } from '@shlack/types'
+import { apiCall } from '@shlack/utils'
 
-const cachedMessageRecordArrays: Record<string, Promise<IMessage[]>> = {};
+const cachedMessageRecordArrays: Record<string, Promise<IMessage[]>> = {}
 
 export async function getChannelMessages(
   teamId: string,
   channelId: string
 ): Promise<IMessage[]> {
-  let cached = cachedMessageRecordArrays[channelId];
-  if (typeof cached === "undefined")
+  let cached = cachedMessageRecordArrays[channelId]
+  if (typeof cached === 'undefined')
     cached = cachedMessageRecordArrays[channelId] = apiCall(
       `teams/${teamId}/channels/${channelId}/messages`
     ).then((rawData) => {
-      debugger;
       if (isTypedArray(rawData, isMessage)) {
-        return rawData;
+        return rawData
       } else
         throw new Error(
           `Unexpected value for message array\n${JSON.stringify(rawData)}`
-        );
-    });
-  return await cached;
+        )
+    })
+  return await cached
 }
